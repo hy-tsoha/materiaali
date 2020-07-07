46730be41c84c650ec05853ffea777d3883a3203
Terho Uotila <terho.uotila@helsinki.fi>
2020-07-07 18:56:09 +0300

    start of custom lexer

---
+++ _plugins/prompt.rb
@@ -0,0 +1,17 @@
+# This "hook" is executed right before the site's pages are rendered
+Jekyll::Hooks.register :site, :pre_render do |site|
+  puts "Registering minimal prompt lexer..."
+  require "rouge"
+
+  class Prompt < Rouge::RegexLexer
+    title "prompt"
+    desc "Minimal terminal prompt"
+
+    tag 'prompt'
+    aliases 'prompt'
+
+    state :root do
+      rule %r/\$\s+/, Name::Variable
+    end
+  end
+end
