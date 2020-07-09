aa23ae871a309f45bdc4082471cc8d3f2983bf81
Terho Uotila <terho.uotila@helsinki.fi>
2020-07-09 12:21:54 +0300

    prompt is always at the start of line

--- _plugins/prompt.rb
+++ _plugins/prompt.rb
@@ -11,10 +11,10 @@ Jekyll::Hooks.register :site, :pre_render do |site|
     aliases 'prompt'
 
     state :root do
-      rule %r/\$\s+/, Name::Variable
-      rule %r/\(venv\)\s+\$\s+/, Name::Variable
-      rule %r/>>>\s+/, Name::Variable
-      rule %r/[a-z]+=#\s+/, Name::Variable
+      rule %r/^\$\s+/, Name::Variable
+      rule %r/^\(venv\)\s+\$\s+/, Name::Variable
+      rule %r/^>>>\s+/, Name::Variable
+      rule %r/^[a-z]+=#\s+/, Name::Variable
       rule %r/.+/, Text
       rule %r/\n/, Text
     end
